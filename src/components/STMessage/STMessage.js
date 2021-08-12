4f28fd8b7701517b3da73890722d9c09774979a4
Geiser <geiser@alumni.usp.br>
2021-08-12 00:40:06 -0300

    adding canvasJS to display barcharts

--- src/components/STMessage/STMessage.js
+++ src/components/STMessage/STMessage.js
@@ -2,24 +2,50 @@ import React from "react";
 import { Box, Button } from "@material-ui/core";
 import Context from "../../contexts/Context";
 
+import CanvasJSReact from './canvasjs.react';
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
 export default class STMessage extends React.Component {
     static contextType = Context;
 
     render() {
         let localization = this.context.environment.localization;
-        
-        return (
-            <Box>
-                <Box>
-                    <img src={localization.stMessage.img} />
-                </Box>
+        let stMessage = localization.stMessage;
 
-                <center style={{margin: "16px"}}>
-                    <Button variant="contained" color="primary" size="large" onClick={this.props.onClickNext}>
-                        {localization.stMessage.next}
-                    </Button>
-                </center>
-            </Box>
-        );
+        if (stMessage.chart != undefined) {
+            const options = {
+                title: { text: stMessage.chart.title },
+                data: [{
+                    type: "column",
+                    dataPoints: stMessage.chart.dataPoints
+                }]
+            };
+
+            return (
+                <Box>
+                    <Box>
+                        <CanvasJSChart options = {options}  onRef = {ref => this.chart = ref} />
+                    </Box>
+    
+                    <center style={{margin: "16px"}}>
+                        <Button variant="contained" color="primary" size="large" onClick={this.props.onClickNext}>
+                            {stMessage.next}
+                        </Button>
+                    </center>
+                </Box>
+            );
+        } else {
+            return(
+                <Box>
+                    <center style={{margin: "16px"}}>
+                        <Button variant="contained" color="primary" size="large" onClick={this.props.onClickNext}>
+                            {stMessage.next}
+                        </Button>
+                    </center>
+                </Box>
+            );
+        }
+
+        
     }
 }
